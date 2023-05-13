bc323e43a042a3b8996dd8b6deee9ec3249ded60
manalan-km <mkumarasamymuru@deakin.edu.au>
2023-05-13 21:41:45 +1000

    added launch file and description for installation

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
-
+from glob import glob
+import os 
 package_name = 'fyp_proj'
 
 setup(
@@ -10,6 +11,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share',package_name,'description'), glob('description/*')),
+        (os.path.join('share',package_name,'launch'), glob('launch/*')),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
