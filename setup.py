ea9a8d562ba17c5da580c5f58e9cd9830ba6ef38
manalan-km <mkumarasamymuru@deakin.edu.au>
2023-05-13 17:38:34 +1000

    first commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'fyp_proj'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='manalan',
+    maintainer_email='mkumarasamymuru@deakin.edu.au',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
