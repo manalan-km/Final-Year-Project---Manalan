db7f35977cfef748240c683b49d162a5489ffd8c
manalan-km <mkumarasamymuru@deakin.edu.au>
2023-05-22 00:13:25 +1000

    removed gazebo_params

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -18,23 +18,31 @@ def generate_launch_description():
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
     package_name='fyp_proj' #<--- CHANGE ME
-
+    package_share = get_package_share_directory(package_name)
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','robot.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
+
+    gazebo_params_path = os.path.join(
+                  package_share,'config','gazebo_params.yaml')
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+            PythonLaunchDescriptionSource([os.path.join(
+                get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                #launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_path }.items()
+         )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'robot'],
+                                   '-entity', 'robot',
+                                    '-x', '21.0',
+                                    '-y', '-17.50',
+                                    '-Y', '3.14'
+                                   ],
                         output='screen')
 
 
